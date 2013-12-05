4a579d0a2a83bfee993d1faf0606899329c6250b
Alvin Berthelot <aberthelot@ippon.fr>
2013-12-05 17:54:06 +0100

    ajout de l'affichage de la recherche

--- views/index.php
+++ views/index.php
@@ -16,23 +16,23 @@
 
 get_header(); ?>
 
-	<div id="primary" class="col-sm-8">
-		<div id="content" class="site-content" role="main">
-		<?php if ( have_posts() ) : ?>
+  <div id="primary" class="col-sm-8">
+    <div id="content" class="site-content" role="main">
+    <?php if (have_posts()) : ?>
 
-			<?php /* The loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
+      <?php /* The loop */ ?>
+      <?php while (have_posts()) : the_post(); ?>
+        <?php get_template_part('content', get_post_format()); ?>
+      <?php endwhile; ?>
 
-			<?php twentythirteen_paging_nav(); ?>
+      <?php twentythirteen_paging_nav(); ?>
 
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-		<?php endif; ?>
+    <?php else : ?>
+      <?php get_template_part('content', 'none'); ?>
+    <?php endif; ?>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+    </div><!-- #content -->
+  </div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
