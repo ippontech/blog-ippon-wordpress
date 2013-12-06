bc7e7f678a3771e26d4083049a15248627ed4959
Alvin Berthelot <aberthelot@ippon.fr>
2013-12-06 12:00:01 +0100

    amélioration archives + redécoupage CSS

--- views/category.php
+++ views/category.php
@@ -13,24 +13,22 @@ get_header(); ?>
 
   <div id="primary" class="col-sm-8 content-area">
     <div id="content" class="site-content" role="main">
+
+    <header class="archive-header">
+      <h1 class="archive-title">
+        <?php echo single_cat_title(); ?>
+      </h1>
+      <?php if ( category_description() ) : // Show an optional category description ?>
+      <p class="archive-meta"><?php echo category_description(); ?></p>
+      <?php endif; ?>
+    </header><!-- .archive-header -->
+
     <?php if (have_posts()) : ?>
-
-      <header class="archive-header">
-        <h1 class="archive-title">
-          <?php echo get_search_query(); ?>
-        </h1>
-        <?php if ( category_description() ) : // Show an optional category description ?>
-        <div class="archive-meta"><?php echo category_description(); ?></div>
-        <?php endif; ?>
-      </header><!-- .archive-header -->
-
       <?php /* The loop */ ?>
       <?php while (have_posts() ) : the_post(); ?>
         <?php get_template_part('content', get_post_format()); ?>
       <?php endwhile; ?>
-
       <?php ippon_paging_nav(); ?>
-
     <?php else : ?>
       <?php get_template_part( 'content', 'none' ); ?>
     <?php endif; ?>
