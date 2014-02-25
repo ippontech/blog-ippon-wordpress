2a6a18a860635d86bf41b8507f56d948e6b7e38d
franckclement <fclement@ippon.fr>
2014-02-25 11:04:41 +0100

    eventbrite ready

--- views/Eventbrite.php
+++ views/Eventbrite.php
@@ -249,8 +249,8 @@ class Eventbrite {
   }
 
   public static function eventListRow( $evnt ) {
-
-    setlocale(LC_TIME, "fr_FR");//Permet un affichage des dates en français.
+    //Permet un affichage des dates en français.
+    setlocale(LC_TIME, "fr_FR");
     $time = strtotime($evnt->start_date);
     $venue_name = 'online';
     if( isset($evnt->venue) && isset( $evnt->venue->name )){
